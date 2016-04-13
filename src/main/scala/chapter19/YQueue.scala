a78bd1aa9b4a1176883fa2a0b6c14918fe68a74f
rhyno.j <rhyno.j@kakaocorp.com>
2016-04-13 19:42:35 +0900

    chapter 19. basic queue refactoring3 (use trait and inner class)

--- src/main/scala/chapter19/YQueue.scala
+++ src/main/scala/chapter19/YQueue.scala
@@ -1,28 +1,31 @@
 package chapter19
 
-class YQueue[T] private (
-  private val leading: List[T],
-  private val trailing: List[T]
-) {
-
-  private def this(elems: T*) = this(elems.toList, Nil)
-
-  private def mirror =
-      if (leading.isEmpty)
-        new YQueue[T](trailing.reverse, leading)
-      else
-        this
-
-  def head = mirror.leading.head
-  def tail = {
-    val q = mirror
-    new YQueue[T](q.leading.tail, q.trailing)
-  }
-
-  def enqueue(x: T) = new YQueue[T](leading, x::trailing)
-  def size: Int = leading.size + trailing.size
+trait YQueue[T] {
+  def head: T
+  def tail: YQueue[T]
+  def enqueue(x: T): YQueue[T]
+  def size: Int
 }
 
 object YQueue {
-  def apply[T](xs: T*) = new YQueue[T](xs.toList, Nil)
+  def apply[T](xs: T*): YQueue[T] = new YQueueImpl[T](xs.toList, Nil)
+
+  private class YQueueImpl[T] (private val leading:List[T], private val trailing:List[T]) extends YQueue[T] {
+    private def this(elems: T*) = this(elems.toList, Nil)
+
+    private def mirror =
+      if (leading.isEmpty)
+        new YQueueImpl[T](trailing.reverse, leading)
+      else
+        this
+
+    override def head = mirror.leading.head
+    override def tail = {
+      val q = mirror
+      new YQueueImpl[T](q.leading.tail, q.trailing)
+    }
+
+    override def enqueue(x: T) = new YQueueImpl[T](leading, x::trailing)
+    override def size: Int = leading.size + trailing.size
+  }
 }
